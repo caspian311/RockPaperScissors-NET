159f07f8b32b73b40cd156c4b6f36dc672159786
Pairing Station <pairing-station@asynchrony.com>
2015-11-20 17:28:57 -0600

    init

---
+++ RockPaperScissors.Test/RockPaperScissorsTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace RockPaperScissors
+{
+    [TestFixture]
+    public class RockPaperScissorsTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var testObject = new RockPaperScissors();
+            testObject.DoThing();
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors.cs
@@ -0,0 +1,10 @@
+﻿namespace RockPaperScissors
+{
+    public class RockPaperScissors
+    {
+        public void DoThing()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
